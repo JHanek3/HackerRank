c1385483805043838ebae2d4967f081bb579c1bf
Jon <>
2021-03-03 12:17:30 -0600

    3/3

---
+++ sql/average_population_of_each_continent.sql
@@ -0,0 +1,7 @@
+-- Task
+-- Given the CITY and COUNTRY tables, query the names of all the continents and their respective city populations rounded down to the nearest integer
+
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION))
+FROM CITY INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT;
